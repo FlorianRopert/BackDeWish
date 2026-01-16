d344240f0f88519940477bbd896b3916cffe38d4
FlorianRopert <ropertflorian.etude@gmail.com>
2026-01-16 11:52:15 +0100

    1er Connection a la bdd

--- public/main.js
+++ public/main.js
@@ -1,16 +1,22 @@
 
 const monInput = document.getElementById('monInput');
 const monBouton = document.getElementById('monBouton');
-
+const monBouton2 = document.getElementById('monBouton2');
 monBouton.addEventListener('click', () => {
     fetch('/register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ inputValue: monInput.value })     
+        body: JSON.stringify({ inputValue: monInput.value })
     }).then(response => response.text())
-      .then(data => {
-          alert(data);
-      });
+        .then(data => {
+            alert(data);
+        });
+});
+
+monBouton2.addEventListener('click', () => {
+    fetch('/info')
+        .then(response => response.json())
+        .then(data => alert(data.cle1));
 });
--- server.js
+++ server.js
@@ -1,23 +1,41 @@
-const express = require("express");
+const express = require('express');
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: '172.29.18.112',
+  user: 'AccessNodeServerBackDeWish',
+  password: 'AccessNodeServerBackDeWish',
+  database: 'BDDtest'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données :', err);
+    return;
+  }
+  console.log('Connecté à la base de données MySQL.');
+});
+
 const app = express();
+app.use(express.static('public'));
+app.use(express.json());
+ 
 
-app.use(express.static("public"));
-
-
-
-app.get("/login", (req, res) => {
-  res.send("Hello, World!");
+app.get('/login', (req, res) => {
+  res.send('<h1>Bienvenue sur la page de login  </h1>');
 });
 
-app.get("/info", (req, res) => {
-  res.json({ cle1: "valeur1", cle2: "valeur2" });
+app.get('/info', (req, res) => {
+  res.json({ cle1: 'valeur1', cle2: 'valeur2' });
+
 });
 
-app.post("/register", (req, res) => {
-  res.send("Brouette");
+app.post('/register', (req, res) => {
+console.log('Données reçues pour l\'inscription');
+console.log(req.body);
+  res.json({ message: 'Inscription réussie !' });
 });
 
 app.listen(3000, () => {
-    let monIP = require("ip").address();
-    console.log(`Server is running on http://${monIP}:3000`);
+  let monIp = require("ip").address();
+  console.log(`Server running on http://${monIp}:3000`);
 });
