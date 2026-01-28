5167a97092bcf7d0395c10bb0e69c3e67a6bd5b5
FlorianRopert <ropertflorian.etude@gmail.com>
2026-01-28 15:28:55 +0100

    Systeme de vote

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const app = express();
 const mysql = require('mysql2');
+
+
 const connection = mysql.createConnection({
   host: '172.29.18.112',
   user: 'AccessNodeServerBackDeWish',
@@ -15,7 +18,6 @@ connection.connect((err) => {
   console.log('Connecté à la base de données MySQL.');
 });
 
-const app = express();
 app.use(express.static('public'));
 app.use(express.json());
  
@@ -28,7 +30,16 @@ app.get('/info', (req, res) => {
   res.json({ cle1: 'valeur1', cle2: 'valeur2' });
 
 });
-
+app.get('/user', (req, res) => {
+connection.query('SELECT * FROM User', (err, results) => {
+if (err) {
+console.error('Erreur lors de la récupération des utilisateurs :', err);
+res.status(500).json({ message: 'Erreur serveur' });
+return;
+}
+res.json(results);
+});
+});
 
 app.post('/register', (req, res) => {
 
@@ -45,6 +56,52 @@ connection.query(
     res.json({ message: 'Inscription réussie !', userId: results.insertId });
   }
 )});
+
+
+
+app.get('/Vote', (req, res) => {
+connection.query('SELECT * FROM Vote', (err, results) => {
+if (err) {
+console.error('Erreur lors de la récupération des votes :', err);
+res.status(500).json({ message: 'Erreur serveur' });
+return;
+}
+res.json(results);
+});
+});
+app.post('/Vote', (req, res) => {
+
+connection.query(
+  'INSERT INTO Vote (id_user) VALUES (?)',
+  [req.body.inputValue],
+  (err, results) => {
+    if (err) {
+      console.error('Erreur lors de l\'insertion dans la base de données :', err);
+      res.status(500).json({ message: 'Erreur serveur' });
+      return;
+    }
+    console.log('Insertion réussie, ID vote :', results.insertId);
+    res.json({ message: 'Vote enregistré !', id_user: results.insertId });
+  }
+)});
+
+
+
+app.get('/VoteCount', (req, res) => {
+connection.query('SELECT User.login, COUNT(Vote.id_user) AS voteCount FROM User, Vote WHERE User.id = Vote.id_user GROUP BY User.id ORDER BY voteCount DESC;',
+(err, results) => {
+if (err) {
+console.error('Erreur lors de la récupération du nombre de votes :', err);
+res.status(500).json({ message: 'Erreur serveur' });
+return;
+}
+res.json(results);
+})
+})
+
+
+
+
 
 app.listen(3000, () => {
   let monIp = require("ip").address();
